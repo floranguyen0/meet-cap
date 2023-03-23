5413fe01ca8dfefc7f4c678d7f21d9894ca07666
FloraNguyen <floranguyen010@gmail.com>
2023-03-23 10:13:59 +0700

    use a fix sol version

--- contracts/contracts-nonupgradable/thedragon/TheDragonPresale.sol
+++ contracts/contracts-nonupgradable/thedragon/TheDragonPresale.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity 0.8.19;
 
 import "../../contracts-upgradable/thedragon/TheDragon.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
